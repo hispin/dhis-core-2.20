535fd53a6335ba80a83c1efd7288b6329ca6d13d
Lars Helge Overland <larshelge@gmail.com>
2015-09-07 14:00:31 +0200

    revno: 19831 message:   Simplified program indicator regex
    committer: Lars Helge Overland <larshelge@gmail.com>
    branch nick: 2.20
    timestamp: Mon 2015-09-07 14:00:31 +0200

--- dhis-2/dhis-api/src/main/java/org/hisp/dhis/program/ProgramIndicator.java
+++ dhis-2/dhis-api/src/main/java/org/hisp/dhis/program/ProgramIndicator.java
@@ -71,11 +71,11 @@ public class ProgramIndicator
     public static final String VAR_ZERO_POS_VALUE_COUNT = "zero_pos_value_count";
     
     public static final String VALUE_TYPE_DATE = "date";
-    public static final String VALUE_TYPE_INT = "int";
-    
-    private static final String EXPRESSION_REGEXP = "(" + KEY_DATAELEMENT + "|" + KEY_ATTRIBUTE + "|" + KEY_PROGRAM_VARIABLE + "|" + KEY_CONSTANT + ")\\{(\\w+|" + 
-        VAR_INCIDENT_DATE + "|" + VAR_ENROLLMENT_DATE + "|" + VAR_CURRENT_DATE + ")" + SEPARATOR_ID + "?(\\w*)\\}";
-    private static final String SQL_FUNC_REGEXP = "d2:(.+?)\\((.+?)\\)";
+    public static final String VALUE_TYPE_INT = "int";    
+
+    public static final String EXPRESSION_PREFIX_REGEXP = KEY_DATAELEMENT + "|" + KEY_ATTRIBUTE + "|" + KEY_PROGRAM_VARIABLE + "|" + KEY_CONSTANT;
+    public static final String EXPRESSION_REGEXP = "(" + EXPRESSION_PREFIX_REGEXP + ")\\{([\\w\\_]+)" + SEPARATOR_ID + "?(\\w*)\\}";
+    public static final String SQL_FUNC_REGEXP = "d2:(.+?)\\((.+?)\\)";
 
     public static final Pattern EXPRESSION_PATTERN = Pattern.compile( EXPRESSION_REGEXP );
     public static final Pattern SQL_FUNC_PATTERN = Pattern.compile( SQL_FUNC_REGEXP );
