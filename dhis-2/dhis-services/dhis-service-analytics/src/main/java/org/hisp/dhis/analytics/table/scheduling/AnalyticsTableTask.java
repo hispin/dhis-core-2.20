6b60602a4e56a4bf98f29ceec21a55704d6352b3
Lars Helge Overland <larshelge@gmail.com>
2015-09-15 19:44:33 +0200

    revno: 19863 message:   R 20171
    committer: Lars Helge Overland <larshelge@gmail.com>
    branch nick: 2.20
    timestamp: Tue 2015-09-15 19:44:33 +0200

--- dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/analytics/table/scheduling/AnalyticsTableTask.java
+++ dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/analytics/table/scheduling/AnalyticsTableTask.java
@@ -29,6 +29,7 @@ package org.hisp.dhis.analytics.table.scheduling;
  */
 
 import static org.hisp.dhis.setting.SystemSettingManager.KEY_LAST_SUCCESSFUL_ANALYTICS_TABLES_UPDATE;
+import static org.hisp.dhis.setting.SystemSettingManager.KEY_LAST_SUCCESSFUL_ANALYTICS_TABLES_RUNTIME;
 import static org.hisp.dhis.system.notification.NotificationLevel.ERROR;
 import static org.hisp.dhis.system.notification.NotificationLevel.INFO;
 import static org.hisp.dhis.setting.SystemSettingManager.KEY_APPLICATION_TITLE;
@@ -44,6 +45,7 @@ import org.hisp.dhis.scheduling.TaskId;
 import org.hisp.dhis.setting.SystemSettingManager;
 import org.hisp.dhis.system.notification.Notifier;
 import org.hisp.dhis.commons.util.TextUtils;
+import org.hisp.dhis.system.util.DateUtils;
 import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -175,5 +177,6 @@ public class AnalyticsTableTask
         }
         
         systemSettingManager.saveSystemSetting( KEY_LAST_SUCCESSFUL_ANALYTICS_TABLES_UPDATE, startTime );
+        systemSettingManager.saveSystemSetting( KEY_LAST_SUCCESSFUL_ANALYTICS_TABLES_RUNTIME, DateUtils.getPrettyInterval( clock.getSplitTime() ) );
     }
 }
